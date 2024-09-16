85a22839b0c58b452595078d967627c94adfda7e
Nick <nickslaven@gmail.com>
2024-09-16 13:14:04 +0100

    updated docs for helper libraries

--- Source/Inkpot/Public/Inkpot/InkpotListLibrary.h
+++ Source/Inkpot/Public/Inkpot/InkpotListLibrary.h
@@ -14,63 +14,185 @@ class UInkpotListLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 public:
 
-	/* prints the contents of the list to a string */
+	/**
+	 * ToString ( Inkpot List )
+	 * prints the contents of the list to a comma delimeted string.
+	 *
+	 * @param Value, name of the list to print.
+	 * @param ReturnValue, the list as a comma delimeted string of item names.
+	 * @param bUseOrigin, should the item names have their origin as a dot delimeted prefix.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|List")
 	static INKPOT_API void ToString(const FInkpotList &Value, FString &ReturnValue, bool bUseOrigin = true );
 
-	/* Create an Ink List from an array of Strings */
+	/**
+	 * MakeInkpotList 
+	 * Creates an Inkpot List consiting of defintion name and array of item names.
+	 * If all items are dot prefixed with the origin name, defintion can be left blank.
+	 * Actual origin reolution happens when the list is passed to the ink story.
+	 * 
+	 * @param Defintion, the origin, this will be the name of the list in the Ink script.
+	 * @param Values, an array of strings for the items. Other list definitions can be included as dot prefixed item names.  
+	 * @return An Inkpotlist. 
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|List")
 	static INKPOT_API FInkpotList MakeInkpotList(FString Definition, TArray<FString> Value);
 
-	/* Create an Ink List from a single string */
+	/**
+	 * MakeInkpotListFromString
+	 * Creates an Inkpot List consiting of defintion name and a comma delimeted string of item names.
+	 * If all items are dot prefixed with the origin name, defintion can be left blank.
+	 * Actual origin reolution happens when the list is passed to the ink story.
+	 *
+	 * @param Defintion, the origin, this will be the name of the list in the Ink script.
+	 * @param Value, commas delimeted list of item names. Other list definitions can be included as dot prefixed item names.
+	 * @return An Inkpotlist.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|List")
 	static INKPOT_API FInkpotList MakeInkpotListFromString(FString Definition, FString Value);
 
-	/* Get an array of strings from an Ink List */
+	/**
+	 * InkpotListAsStrings
+	 * Converts an inkpot list to an array of strings.
+	 *
+	 * @param Value, list to convert.
+	 * @param ReturnValue, the list as a string array of item names.
+	 * @param bUseOrigin, should the item names have their origin as a dot delimeted prefix.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|List")
 	static INKPOT_API void InkpotListAsStrings(const FInkpotList &Value, TArray<FString> &ReturnValue, bool bUseOrigin = true );
 
-	/* Returns a new list that is the combination of both lists passed in. Equivalent to calling (list1 + list2) in ink. */
+	/**
+	 * Union
+	 * Returns a new list that is the combination of both lists passed in. 
+	 * Equivalent to calling (A + B) in ink..
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return A new Inkpotlist.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Union", CommutativeAssociativeBinaryOperator = "true", CompactNodeTitle = "+"), Category="Inkpot|List")
 	static INKPOT_API FInkpotList Union(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns a new list that is the intersection of both lists passed in - i.e. a list of the items that are shared between the two lists. Equivalent to calling (list1 ^ list2) in ink. */
+	/**
+	 * Intersect
+	 * Returns a new list that is the intersection of both lists passed in 
+	 * - i.e. a list of the items that are shared between the two lists. 
+	 * Equivalent to calling (A ^ B) in ink.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return A new Inkpotlist.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Intersect", CompactNodeTitle = "^"), Category="Inkpot|List")
 	static INKPOT_API FInkpotList Intersect(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns true if there is an intersection of both lists passed in - i.e. both lists share some items. */
+	/**
+	 * HasIntersection
+	 * Returns true if there is an intersection of both lists passed in 
+	 * - i.e. both lists share some items
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if there is an intersection, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Has Intersection", CompactNodeTitle = "(^)"), Category="Inkpot|List")
 	static INKPOT_API bool HasIntersection(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns a new list that is the first list with items in the second list removed. Equivalent to calling (list1 - list2) in ink. */
+	/**
+	 * Without
+	 * Returns a new list that is the first list with items in the second list removed. 
+	 * Equivalent to calling (A - B) in ink.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return A new Inkpotlist.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Without", CompactNodeTitle = "-"), Category="Inkpot|List")
 	static INKPOT_API FInkpotList Without( const FInkpotList &A, const FInkpotList &B );
 
-	/* Returns true if the the first list contains all the items that are in the second list. Equivalent to calling (list1 ? list2) in ink. */
+	/**
+	 * ContainsList
+	 * Returns true if the the first list contains all the items that are in the second list. 
+	 * Equivalent to calling (A ? B) in ink.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A contains B, false otherwise.
+	 */
 	UFUNCTION( BlueprintPure, meta = (DisplayName = "Contains List", CompactNodeTitle = "?"), Category = "Inkpot|List" )
 	static INKPOT_API bool ContainsList( const FInkpotList &Source, const FInkpotList &Querant );
 
-	/* Returns true if the list contains an item matching the given name. */
+	/*  */
+	/**
+	 * ContainsList
+	 * Returns true if the list contains an item matching the given name.
+	 *
+	 * @param Source, InkpotList to test.
+	 * @param ItemName, string, item name.
+	 * @return true if Source contains ItemName, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Contains Item", CompactNodeTitle = "?"), Category="Inkpot|List")
 	static INKPOT_API bool ContainsItem(const FInkpotList &Source, const FString &ItemName);
 
-	/* Returns true if all the item values in the first list are greater than all the item values in the second list. Equivalent to calling (list1 > list2) in ink. */
+	/**
+	 * GreaterThan
+	 * Returns true if all the item values in the first list are greater than all the item values in the second list. 
+	 * Equivalent to calling (A > B) in ink.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A > B, false otherwise.
+	 */
 	UFUNCTION( BlueprintPure, meta = (DisplayName = "Greater Than", CompactNodeTitle = ">"), Category = "Inkpot|List" )
 	static INKPOT_API bool GreaterThan(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns true if the item values in the first list overlap or are all greater than the item values in the second list. None of the item values in the current list must fall below the item values in the passed in list. Equivalent to (list1 >= list2) in ink, or LIST_MIN(list1) >= LIST_MIN(list2) &amp;&amp; LIST_MAX(list1) >= LIST_MAX(list2). */
+	/**
+	 * GreaterThanOrEquals
+	 * Returns true if the item values in the first list overlap or are all greater than the item values in the second list. 
+	 * None of the item values in the current list must fall below the item values in the passed in list. 
+	 * Equivalent to (A >= B) in ink, or LIST_MIN(A) >= LIST_MIN(B) && LIST_MAX(A) >= LIST_MAX(B).
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A >= B, false otherwise.
+	 */
 	UFUNCTION( BlueprintPure, meta = (DisplayName = "Greater than or equals", CompactNodeTitle = ">="), Category = "Inkpot|List" )
 	static INKPOT_API bool GreaterThanOrEquals(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns true if all the item values first list are less than all the item values in the second list. Equivalent to calling (list1 < list2) in ink. */
+	/**
+	 * LessThan
+	 * Returns true if all the item values first list are less than all the item values in the second list. 
+	 * Equivalent to calling (A < B) in ink.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A < B, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Less than", CompactNodeTitle = "<"), Category="Inkpot|List")
 	static INKPOT_API bool LessThan(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns true if the item values in the first list overlap or are all less than the item values in the second list. None of the item values in the current list must go above the item values in the passed in list. Equivalent to (list1 <= list2) in ink, or LIST_MAX(list1) <= LIST_MAX(list2) && LIST_MIN(list1) <= LIST_MIN(list2). */
+	/**
+	 * LessThanOrEquals
+	 * Returns true if the item values in the first list overlap or are all less than the item values in the second list. 
+	 * None of the item values in the current list must go above the item values in the passed in list. 
+	 * Equivalent to (A <= B) in ink, or LIST_MAX(A) <= LIST_MAX(B) && LIST_MIN(A) <= LIST_MIN(B).
+	 * 
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A <= B, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Less than or equals", CompactNodeTitle = "<="), Category="Inkpot|List")
 	static INKPOT_API bool LessThanOrEquals(const FInkpotList &A, const FInkpotList &B);
 
-	/* Returns true if the both lists contain the same items, false otherwise. */
+	/**
+	 * Equals
+	 * Returns true if the both lists contain the same items, false otherwise.
+	 *
+	 * @param A, InkpotList.
+	 * @param B, InkpotList.
+	 * @return true if A == B, false otherwise.
+	 */
 	UFUNCTION(BlueprintPure, meta=(DisplayName = "Equals", CompactNodeTitle = "=="), Category="Inkpot|List")
 	static INKPOT_API bool Equals(const FInkpotList &A, const FInkpotList &B);
 };
--- Source/Inkpot/Public/Inkpot/InkpotValueLibrary.h
+++ Source/Inkpot/Public/Inkpot/InkpotValueLibrary.h
@@ -13,63 +13,159 @@ class UInkpotValueLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	/* Deafult value for returning from functions */
+	/**
+	 * DefaultInkpotValue
+	 * Default value for returning from functions.
+	 * 
+	 * @return an empty inkpot value.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotValue DefaultInkpotValue();
 
-	/* Create an Ink Value from a boolean */
+	/**
+	 * MakeBoolInkpotValue
+	 * Create an Inkpot Value from a boolean.
+	 *
+	 * @param bValue, the boolean to convert.
+	 * @return a new inkpot value.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotValue MakeBoolInkpotValue(bool bValue);
 
-	/* Cast Ink value to a boolean */
+	/**
+	 * InkpotValueAsBool
+	 * Cast an Inkpot value to a boolean.
+	 *
+	 * @param Value, the Inkpot value to convert.
+	 * @return a boolean.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API bool InkpotValueAsBool(const FInkpotValue &Value);
 
-	/* Cast Ink array value to a boolean */
+	/**
+	 * InkpotArrayValueAsBool
+	 * Cast an Inkpot array value to a boolean.
+	 *
+	 * @param Values, the array of Inkpot values.
+	 * @param Index, the indexed value in the array to convert.
+	 * @return a boolean.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API bool InkpotArrayValueAsBool(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/* Create an Ink Value from an integer */
+	/**
+	 * MakeIntInkpotValue
+	 * Create an Inkpot Value from an integer.
+	 *
+	 * @param Value, the integer to convert.
+	 * @return a new inkpot value.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotValue MakeIntInkpotValue(int32 Value);
 
-	/* Cast Ink value to an integer*/
+	/**
+	 * InkpotValueAsInt
+	 * Cast Inkpot value to an integer.
+	 *
+	 * @param Value, the Inkpot value to convert.
+	 * @return an integer.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API int32 InkpotValueAsInt(const FInkpotValue &Value);
 
-	/* Cast Ink array value to an integer */
+	/**
+	 * InkpotArrayValueAsInt
+	 * Cast an Inkpot array value to an integer.
+	 *
+	 * @param Values, the array of Inkpot values.
+	 * @param Index, the indexed value in the array to convert.
+	 * @return an integer.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API int32 InkpotArrayValueAsInt(const TArray<FInkpotValue> &Values, int Index = 0);
 
-	/* Create an Ink Value from a float */
+	/**
+	 * MakeFloatInkpotValue
+	 * Create an Inkpot value from a floating point number.
+	 *
+	 * @param Value, the floating point number to convert.
+	 * @return a new inkpot value.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotValue MakeFloatInkpotValue(float Value);
 
-	/* Cast Ink value to a float*/
+	/**
+	 * InkpotValueAsFloat
+	 * Cast Inkpot value to a floating point number.
+	 *
+	 * @param Value, the Inkpot value to convert.
+	 * @return a float.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API float InkpotValueAsFloat(const FInkpotValue &Value);
 
-	/* Cast Ink array value to a float */
+	/**
+	 * InkpotArrayValueAsFloat
+	 * Cast an Inkpot array value to a floating point number.
+	 *
+	 * @param Values, the array of Inkpot values.
+	 * @param Index, the indexed value in the array to convert.
+	 * @return a float.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API float InkpotArrayValueAsFloat(const TArray<FInkpotValue> &Values, int Index = 0);
 
-	/* Create an Ink Value from a string */
+	/**
+	 * MakeStringInkpotValue
+	 * Create an Inkpot value from a string.
+	 *
+	 * @param Value, the string to convert.
+	 * @return a new inkpot value.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotValue MakeStringInkpotValue(const FString &Value);
 
-	/* Cast Ink value to a string*/
+	/**
+	 * InkpotValueAsString
+	 * Cast Ink value to a string.
+	 *
+	 * @param Value, the Inkpot value to convert.
+	 * @return a string.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FString InkpotValueAsString(const FInkpotValue &Value);
 
-	/* Cast Ink array value to a string */
+	/**
+	 * InkpotArrayValueAsString
+	 * Cast an Inkpot array value to a string.
+	 *
+	 * @param Values, the array of Inkpot values.
+	 * @param Index, the indexed value in the array to convert.
+	 * @return a string.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FString InkpotArrayValueAsString(const TArray<FInkpotValue> &Values, int Index = 0);
 
-	/* Cast Inkpot value to an InkpotList */
+	/**
+	 * InkpotValueAsList
+	 * Cast Inkpot value to an InkpotList.
+	 *
+	 * @param Value, the Inkpot value to convert.
+	 * @return an InkpotList.
+	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotList InkpotValueAsList( const FInkpotValue &Value );
 
-	/* Cast Ink array value to an InkpotList */
+	/**
+	 * InkpotArrayValueAsList
+	 * Cast Inkpot array value to an InkpotList.
+	 *
+	 * @param Values, the array of Inkpot values.
+	 * @param Index, the indexed value in the array to convert.
+	 * @return an InkpotList.
+	 */
+
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotList InkpotArrayValueAsList( const TArray<FInkpotValue> &Values, int Index = 0 );
 };
