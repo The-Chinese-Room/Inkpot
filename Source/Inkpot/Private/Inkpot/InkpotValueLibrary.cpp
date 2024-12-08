b6f01005e0ab85212444d3f95d58029bae7eb0f5
TCR-Nick <141139646+TCR-Nick@users.noreply.github.com>
2024-12-08 15:38:45 +0000

    added library functions to test the type of an inkpot value

--- Source/Inkpot/Private/Inkpot/InkpotValueLibrary.cpp
+++ Source/Inkpot/Private/Inkpot/InkpotValueLibrary.cpp
@@ -6,12 +6,22 @@ FInkpotValue UInkpotValueLibrary::MakeBoolInkpotValue(bool bInValue)
 	return FInkpotValue( MakeShared<Ink::FValueType>(bInValue) );
 }
 
+bool UInkpotValueLibrary::IsInkpotValueBool(const FInkpotValue &InValue)
+{
+	return (*InValue)->HasSubtype<bool>();
+}
+
 bool UInkpotValueLibrary::InkpotValueAsBool(const FInkpotValue &InValue)
 {
-	if( (*InValue)->HasSubtype<bool>() )
+	if( IsInkpotValueBool(InValue) )
 		return (*InValue)->GetSubtype<bool>();
 	INKPOT_ERROR( "Value is not a bool, returning default (false)");
 	return false;
+}
+
+bool UInkpotValueLibrary::IsInkpotArrayValueBool(const TArray<FInkpotValue> &InValues, int InIndex )
+{
+	return IsInkpotValueBool( InValues[InIndex] );
 }
 
 bool UInkpotValueLibrary::InkpotArrayValueAsBool(const TArray<FInkpotValue> &InValues, int InIndex )
@@ -24,12 +34,22 @@ FInkpotValue UInkpotValueLibrary::MakeIntInkpotValue(int32 InValue)
 	return FInkpotValue( MakeShared<Ink::FValueType>(InValue) );
 }
 
+bool UInkpotValueLibrary::IsInkpotValueInt(const FInkpotValue &InValue)
+{
+	return (*InValue)->HasSubtype<int32>();
+}
+
 int32 UInkpotValueLibrary::InkpotValueAsInt(const FInkpotValue & InValue)
 {
-	if( (*InValue)->HasSubtype<int32>() )
+	if( IsInkpotValueInt(InValue) )
 		return (*InValue)->GetSubtype<int32>();
 	INKPOT_ERROR( "Value is not a int, returing default (0)");
 	return 0;
+}
+
+bool UInkpotValueLibrary::IsInkpotArrayValueInt(const TArray<FInkpotValue> &InValues, int InIndex)
+{
+	return IsInkpotValueInt( InValues[InIndex] );
 }
 
 int32 UInkpotValueLibrary::InkpotArrayValueAsInt(const TArray<FInkpotValue> &InValues, int InIndex )
@@ -42,14 +62,24 @@ FInkpotValue UInkpotValueLibrary::MakeFloatInkpotValue(float InValue)
 	return FInkpotValue( MakeShared<Ink::FValueType>(InValue) );
 }
 
+bool UInkpotValueLibrary::IsInkpotValueFloat(const FInkpotValue &InValue)
+{
+	return (*InValue)->HasSubtype<float>();
+}
+
 float UInkpotValueLibrary::InkpotValueAsFloat(const FInkpotValue &InValue)
 {
-	if( (*InValue)->HasSubtype<float>() )
+	if( IsInkpotValueFloat(InValue) )
 		return (*InValue)->GetSubtype<float>();
-	else if( (*InValue)->HasSubtype<int32>() )
+	else if( IsInkpotValueInt(InValue) )
 		return (float)((*InValue)->GetSubtype<int32>());
 	INKPOT_ERROR( "Value is not a float, returing default (0.0f)");
 	return 0.0f;
+}
+
+bool UInkpotValueLibrary::IsInkpotArrayValueFloat(const TArray<FInkpotValue> &InValues, int InIndex)
+{
+	return IsInkpotValueFloat( InValues[InIndex] );
 }
 
 float UInkpotValueLibrary::InkpotArrayValueAsFloat(const TArray<FInkpotValue> &InValues, int InIndex )
@@ -62,12 +92,22 @@ FInkpotValue UInkpotValueLibrary::MakeStringInkpotValue(const FString &InValue)
 	return FInkpotValue( MakeShared<Ink::FValueType>(InValue) );
 }
 
+bool UInkpotValueLibrary::IsInkpotValueString(const FInkpotValue &InValue)
+{
+	return (*InValue)->HasSubtype<FString>();
+}
+
 FString UInkpotValueLibrary::InkpotValueAsString(const FInkpotValue &InValue)
 {
-	if( (*InValue)->HasSubtype<FString>() )
+	if( IsInkpotValueString(InValue) )
 		return (*InValue)->GetSubtype<FString>();
 	INKPOT_ERROR( "Value is not a string, returing default (\"\")" );
 	return FString();
+}
+
+bool UInkpotValueLibrary::IsInkpotArrayValueString(const TArray<FInkpotValue> &InValues, int InIndex)
+{
+	return IsInkpotValueString( InValues[InIndex] );
 }
 
 FString UInkpotValueLibrary::InkpotArrayValueAsString(const TArray<FInkpotValue> &InValues, int InIndex )
@@ -75,14 +115,24 @@ FString UInkpotValueLibrary::InkpotArrayValueAsString(const TArray<FInkpotValue>
 	return InkpotValueAsString( InValues[InIndex] );
 }
 
+bool UInkpotValueLibrary::IsInkpotValueList(const FInkpotValue &InValue )
+{
+	return (*InValue)->HasSubtype<Ink::FInkList>();
+}
+
 FInkpotList UInkpotValueLibrary::InkpotValueAsList( const FInkpotValue &InValue )
 {
 	FInkpotList list;
-	if( (*InValue)->HasSubtype<Ink::FInkList>() )
+	if( IsInkpotValueList(InValue) )
 		list = *InValue;
 	else
 		INKPOT_ERROR( "Value is not an ink list, returing default ()" );
 	return list;
+}
+
+bool UInkpotValueLibrary::IsInkpotArrayValueList(const TArray<FInkpotValue> &InValues, int InIndex)
+{
+	return IsInkpotValueList( InValues[InIndex] );
 }
 
 FInkpotList UInkpotValueLibrary::InkpotArrayValueAsList( const TArray<FInkpotValue> &InValues, int InIndex )
--- Source/Inkpot/Public/Inkpot/InkpotValueLibrary.h
+++ Source/Inkpot/Public/Inkpot/InkpotValueLibrary.h
@@ -33,6 +33,16 @@ public:
 	static INKPOT_API FInkpotValue MakeBoolInkpotValue(bool bValue);
 
 	/**
+	 * IsInkpotValueBool
+	 * Tests if the Value is a boolean.
+	 *
+	 * @param Value - The Inkpot value to test.
+	 * @returns true if the value is a a boolean.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotValueBool(const FInkpotValue &Value);
+
+	/**
 	 * InkpotValueAsBool
 	 * Cast an Inkpot value to a boolean.
 	 *
@@ -41,6 +51,17 @@ public:
 	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API bool InkpotValueAsBool(const FInkpotValue &Value);
+
+	/**
+	 * IsInkpotArrayValueBool
+	 * Tests if the array Value is a boolean.
+	 *
+	 * @param Values - The array of Inkpot values.
+	 * @param Index - The indexed value in the array to test.
+	 * @returns true if the value is a a boolean.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotArrayValueBool(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
 	 * InkpotArrayValueAsBool
@@ -53,8 +74,7 @@ public:
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API bool InkpotArrayValueAsBool(const TArray<FInkpotValue> &Values, int Index = 0);
 
-	/* Create an Ink Value from an integer */
-	/**
+	/*
 	 * MakeIntInkpotValue
 	 * Create an Inkpot Value from an integer.
 	 *
@@ -65,6 +85,16 @@ public:
 	static INKPOT_API FInkpotValue MakeIntInkpotValue(int32 Value);
 
 	/**
+	 * IsInkpotValueInt
+	 * Tests if the Value is an integer.
+	 *
+	 * @param Value - The Inkpot value to test.
+	 * @returns true if the value is an integer.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotValueInt(const FInkpotValue &Value);
+
+	/**
 	 * InkpotValueAsInt
 	 * Cast Inkpot value to an integer.
 	 *
@@ -73,6 +103,17 @@ public:
 	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API int32 InkpotValueAsInt(const FInkpotValue &Value);
+
+	/**
+	 * IsInkpotArrayValueInt
+	 * Tests if the array Value is an integer.
+	 *
+	 * @param Values - The array of Inkpot values.
+	 * @param Index - The indexed value in the array to test.
+	 * @returns true if the value is an int.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotArrayValueInt(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
 	 * InkpotArrayValueAsInt
@@ -96,6 +137,17 @@ public:
 	static INKPOT_API FInkpotValue MakeFloatInkpotValue(float Value);
 
 	/**
+	 * IsInkpotValueFloat
+	 * Tests if the Value is a floating point number.
+	 *
+	 * @param Value - The Inkpot value to test.
+	 * @returns true if the value is a float.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotValueFloat(const FInkpotValue &Value);
+
+
+	/**
 	 * InkpotValueAsFloat
 	 * Cast Inkpot value to a floating point number.
 	 *
@@ -104,6 +156,17 @@ public:
 	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API float InkpotValueAsFloat(const FInkpotValue &Value);
+
+	/**
+	 * IsInkpotArrayValueFloat
+	 * Tests if the array Value is a floating point value.
+	 *
+	 * @param Values - The array of Inkpot values.
+	 * @param Index - The indexed value in the array to test.
+	 * @returns true if the value is a float.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotArrayValueFloat(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
 	 * InkpotArrayValueAsFloat
@@ -127,6 +190,16 @@ public:
 	static INKPOT_API FInkpotValue MakeStringInkpotValue(const FString &Value);
 
 	/**
+	 * IsInkpotValueString
+	 * Tests if the Value is a string.
+	 *
+	 * @param Value - The Inkpot value to test.
+	 * @returns true if the value is a string.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotValueString(const FInkpotValue &Value);
+
+	/**
 	 * InkpotValueAsString
 	 * Cast Ink value to a string.
 	 *
@@ -135,6 +208,17 @@ public:
 	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FString InkpotValueAsString(const FInkpotValue &Value);
+
+	/**
+	 * IsInkpotArrayValueString
+	 * Tests if the array Value is a string.
+	 *
+	 * @param Values - The array of Inkpot values.
+	 * @param Index - The indexed value in the array to test.
+	 * @returns true if the value is a string.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotArrayValueString(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
 	 * InkpotArrayValueAsString
@@ -148,6 +232,16 @@ public:
 	static INKPOT_API FString InkpotArrayValueAsString(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
+	 * IsInkpotValueList
+	 * Tests if the Value is a List.
+	 *
+	 * @param Value - The Inkpot value to test.
+	 * @returns true if the value is a List.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotValueList(const FInkpotValue &Value);
+
+	/**
 	 * InkpotValueAsList
 	 * Cast Inkpot value to an InkpotList.
 	 *
@@ -156,6 +250,17 @@ public:
 	 */
 	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
 	static INKPOT_API FInkpotList InkpotValueAsList( const FInkpotValue &Value );
+
+	/**
+	 * IsInkpotArrayValueList
+	 * Tests if the array Value is a list.
+	 *
+	 * @param Values - The array of Inkpot values.
+	 * @param Index - The indexed value in the array to test.
+	 * @returns true if the value is a list.
+	 */
+	UFUNCTION(BlueprintPure, Category="Inkpot|Value")
+	static INKPOT_API bool IsInkpotArrayValueList(const TArray<FInkpotValue> &Values, int Index = 0);
 
 	/**
 	 * InkpotArrayValueAsList
